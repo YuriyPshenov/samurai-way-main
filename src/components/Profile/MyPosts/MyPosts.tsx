d64f464038971cc9f60af43fd5ea251a8bddc1c9
YuriyPshenov <yuriypshenov@gmail.com>
2022-11-21 19:48:46 +0300

    samurai-way-main-lesson-16

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -6,15 +6,15 @@ import Post from "./Post/Post";
 const MyPosts = () => {
     return (
         <div className={s.myPosts}>
-            My posts
+            User's posts
             <div className={s.textAreaZone}>
                 <textarea></textarea>
                 <button>Add post</button>
             </div>
             <div>
-                <Post/>
-                <Post/>
-                <Post/>
+                <Post message={'Hi, how are you?'}/>
+                <Post message={`It's my first post`}/>
+                <Post message={'?'}/>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import './Post.module.css';
 import s from './Post.module.css'
 
-const Post = () => {
+type PostPropsType = {
+    message: string
+}
+
+const Post = (props: PostPropsType) => {
+    const {message} = props
     return (
         <div className={s.post}>
             <img src="https://cdn-icons-png.flaticon.com/512/147/147142.png" alt="user's avatar"/>
-            <div className={s.item}>post 1</div>
+            <div className={s.item}>{message}</div>
+            <span>like</span>
         </div>
     )
 }
