7811f2c529c5ed77570caaca56c3ff0109c4e440
YuriyPshenov <yuriypshenov@gmail.com>
2022-11-21 22:57:17 +0300

    samurai-way-main-lesson-22

--- src/components/Navbar/Dialogs/Dialogs.tsx
+++ src/components/Navbar/Dialogs/Dialogs.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './Dialogs.module.css';
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog + ' ' + s.active}>Dimych</div>
-                <div className={s.dialog}>Andrey</div>
-                <div className={s.dialog}>Sveta</div>
-                <div className={s.dialog}>Masha</div>
-                <div className={s.dialog}>Viktor</div>
+                <div className={s.dialog + ' ' + s.active}><NavLink to={'/dialogs/1'}>Dimych</NavLink></div>
+                <div className={s.dialog}><NavLink to={'/dialogs/2'}>Andrey</NavLink></div>
+                <div className={s.dialog}><NavLink to={'/dialogs/3'}>Sveta</NavLink></div>
+                <div className={s.dialog}><NavLink to={'/dialogs/4'}>Masha</NavLink></div>
+                <div className={s.dialog}><NavLink to={'/dialogs/5'}>Viktor</NavLink></div>
+                <div className={s.dialog}><NavLink to={'/dialogs/6'}>Valera</NavLink></div>
             </div>
             <div className={s.messages}>
                 <div className={s.message}>Hi</div>
