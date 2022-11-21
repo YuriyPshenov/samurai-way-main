d13da45f65f427f22ca6f87a29a359d27cda404c
YuriyPshenov <yuriypshenov@gmail.com>
2022-11-21 17:08:54 +0300

    samurai-way-main-lesson-11

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import './App.css';
-import Technologies from "./Technologies";
-import Footer from "./Footer";
-import Header from "./Header";
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
-            <Footer/>
+        <div className={'app-wrapper'}>
+            <header className={'header'}>
+                <img src="https://www.edigitalagency.com.au/wp-content/uploads/Twitter-logo-png.png" alt={'logo'}/>
+            </header>
+            <nav className={'nav'}>
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>Music</div>
+                <div>News</div>
+            </nav>
+            <div className={'content'}>
+                <div>
+                    <img src="https://static-cse.canva.com/blob/560385/benjaminhung340384unsplash.jpg"
+                         alt="img content"/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>Post</div>
+                </div>
+            </div>
         </div>
     )
 }
